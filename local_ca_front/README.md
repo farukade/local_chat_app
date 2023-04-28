0bc75881b04fa0c3a3d6f6eb30c973235076a072
tylerjusfly <tylerjusfly1@gmail.com>
2023-04-28 13:01:31 +0100

    setting up react and tailwind 🚁🚁

---
+++ local_ca_front/README.md
@@ -0,0 +1,3 @@
+# Getting Started with Local Chat Frontend
+
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
---
+++ local_ca_front/src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import AlertError from "./Common/Alerts/AlertError";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <AlertError />
+      </header>
+    </div>
+  );
+}
+
+export default App;
