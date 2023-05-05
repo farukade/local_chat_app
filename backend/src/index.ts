51f956e5bfa0ec01367498ffe884a869ac37e6a5
farukade <farukadekunle@gmail.com>
2023-05-05 22:45:06 +0100

    socket io resource added on index.js

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,14 +1,19 @@
 import express, { Express } from 'express';
+import http from 'http';
 import { config } from 'dotenv';
 import bodyParser from 'body-parser';
 import allRoutes from './routers';
 import { dbConnection } from './models';
 import cors from 'cors';
 import { requsetLogger } from './middlewares/request.logger';
+import { Server, Socket } from 'socket.io';
 config();
 
 export const app: Express = express();
 const port = process.env.PORT || 3000;
+
+// const activeUsers = {};
+// const activeUsersArr = [];
 
 
 dbConnection();
@@ -27,8 +32,22 @@ app.use(requsetLogger);
 
 app.use(allRoutes);
 
-app.listen(port, () => {
+const server = http.createServer(app);
+const io = new Server(server);
+
+io
+  .of('/chat')
+  .on('connection', (socket: Socket) => {
+    console.log('new user', socket.id, 'connected');
+
+    socket.on('disconnect', () => {
+      console.log('user', socket.id, 'user disconnected');
+    });
+  });
+
+server.listen(port, () => {
   console.log(`⚡️[server]: Server listening on port ${port && +port}`);
 });
+
 
 export default app;
